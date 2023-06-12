b11032b2275f4d759083b5d8b78369a769f1a10a
Shiki Nishino <105764723+Snishino93@users.noreply.github.com>
2023-06-12 01:40:46 +0000

    end

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext.js";
 import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
-import MikePhoto from "../../img/m101.jpg";
+import MikePhoto from "../../img/rigo-baby.jpg";
 
 export const ContactCard = props => {
-	const [state, setState] = useState({
-		//initialize state here
-	});
+	const [state, setState] = useState({});
+
+	const { actions } = useContext(Context);
 
 	return (
 		<li className="list-group-item">
@@ -16,17 +17,17 @@ export const ContactCard = props => {
 				</div>
 				<div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
 					<div className=" float-right">
-						<button className="btn">
+						<button className="btn" onClick={() => props.edit()}>
 							<i className="fas fa-pencil-alt mr-3" />
 						</button>
 						<button className="btn" onClick={() => props.onDelete()}>
 							<i className="fas fa-trash-alt" />
 						</button>
 					</div>
-					<label className="name lead">Mike Anamendolla</label>
+					<label className="name lead">{props.full_name}</label>
 					<br />
 					<i className="fas fa-map-marker-alt text-muted mr-3" />
-					<span className="text-muted">5842 Hillcrest Rd</span>
+					<span className="text-muted">{props.address}</span>
 					<br />
 					<span
 						className="fa fa-phone fa-fw text-muted mr-3"
@@ -34,7 +35,7 @@ export const ContactCard = props => {
 						title=""
 						data-original-title="(870) 288-4149"
 					/>
-					<span className="text-muted small">(870) 288-4149</span>
+					<span className="text-muted small">{props.phone}</span>
 					<br />
 					<span
 						className="fa fa-envelope fa-fw text-muted mr-3"
@@ -42,7 +43,7 @@ export const ContactCard = props => {
 						data-original-title=""
 						title=""
 					/>
-					<span className="text-muted small text-truncate">mike.ana@example.com</span>
+					<span className="text-muted small text-truncate">{props.email}</span>
 				</div>
 			</div>
 		</li>
@@ -55,7 +56,13 @@ export const ContactCard = props => {
  **/
 ContactCard.propTypes = {
 	history: PropTypes.object,
-	onDelete: PropTypes.func
+	onDelete: PropTypes.func,
+	full_name: PropTypes.string,
+	phone: PropTypes.string,
+	email: PropTypes.string,
+	address: PropTypes.string,
+	id: PropTypes.string,
+	edit: PropTypes.func
 };
 
 /**
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,69 @@
+import { objectOf } from "prop-types";
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
+			contacts: []
 			//Your data structures, A.K.A Entities
 		},
 		actions: {
-			//(Arrow) Functions that update the Store
-			// Remember to use the scope: scope.state.store & scope.setState()
+			//-----AÑADIR CONTACTO-----
+			addContact: (fullName, email, phone, address) => {
+				let _datos = {
+					full_name: fullName,
+					email: email,
+					agenda_slug: "snishino",
+					address: address,
+					phone: phone
+				};
+				fetch("https://assets.breatheco.de/apis/fake/contact/", {
+					method: "POST",
+					headers: { "Content-type": "application/json; charset=UTF-8" },
+					body: JSON.stringify(_datos)
+				})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(err => console.log(err));
+			},
+
+			//-----OBTENER CONTACTOS-----
+			searchContact: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/snishino")
+					.then(response => response.json())
+					.then(data => setStore({ contacts: data }))
+					.catch(err => console.log("Request Failed", err));
+			},
+
+			//DELETE CONTACTS
+			onDelete: id => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "DELETE",
+					headers: { "Content-type": "application/json; charset=UTF-8" }
+				})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(err => console.log(err));
+			},
+
+			//EDITAR CONTACTOS
+			editContact: (id, fullName, email, phone, address) => {
+				let _datos = {
+					full_name: fullName,
+					email: email,
+					agenda_slug: "snishino",
+					address: address,
+					phone: phone
+				};
+
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "PUT",
+					body: JSON.stringify(_datos),
+					headers: { "Content-type": "application/json; charset=UTF-8" }
+				})
+					.then(response => response.json())
+					.then(data => console.log(data))
+					.catch(err => console.log(err));
+			}
 		}
 	};
 };
